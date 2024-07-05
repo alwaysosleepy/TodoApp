ea5a8ea409aa9afe9b410f5e1563775ac7fd8c7d
Daria Khartova <khartovad@gmail.com>
2024-07-06 01:39:06 +0200

    added docstrings and fixed compose's code

--- app/src/main/java/dev/dariakhartova/todoapp/domain/usecase/UpdateTodoItemUseCase.kt
+++
@@ -1,9 +0,0 @@
-package dev.dariakhartova.todoapp.domain.usecase
-
-import dev.dariakhartova.todoapp.data.model.TodoItem
-import dev.dariakhartova.todoapp.data.repository.TodoItemRepository
-
-class UpdateTodoItemUseCase(private val repository: TodoItemRepository) {
-
-    suspend operator fun invoke(todoItem: TodoItem) = repository.updateTodoItem(todoItem)
-}
